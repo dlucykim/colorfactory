9ee5929a43265c65814ab91a32b1d4e8b79ab05b
dlucykim <dlucykim@gmail.com>
2022-01-04 22:28:34 +0900

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -209,3 +209,19 @@ window.confettiful = new Confettiful(document.querySelector('.js-container'));
 //   e.target.elements.email.value = '';
 //   e.target.elements.message.value = '';
 // });
+
+
+window.addEventListener("load",function(){
+  setTimeout(
+    function open(event){
+      document.querySelector(".popup").style.
+      display ="block";
+    },
+    2000
+  )
+});
+
+document.querySelector("#close").addEventListener
+("click",function(){
+  document.querySelector(".popup").style.display="none";
+});
